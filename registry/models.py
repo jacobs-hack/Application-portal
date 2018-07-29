a3c312505a85e99855d4b9655569fa321dc3b638
Tom Wiesing <tkw01536@gmail.com>
2018-07-29 11:55:30 -0400

    Backport Announcement model from upstream
    This commit implements the announcement model from upstream Alumni
    MemeberManagement.

--- registry/models.py
+++ registry/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Announcement(models.Model):
+    active = models.BooleanField(help_text="Show Announcement")
+    title = models.TextField(help_text="Title (Text)")
+    content = models.TextField(help_text="Content (HTML)")
+
+    def __str__(self):
+        return "Announcement {}{}".format(repr(self.title), "" if self.active else " (Inactive)")
